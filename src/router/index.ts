da4d1e38acb1758b0bbdc96120e3b0ffc044bb44
Huxzhi <huxzhi@gmail.com>
2023-01-15 15:41:43 +0800

    路由重定向和别名
    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,11 +11,20 @@ import { createRouter, createWebHistory, createWebHashHistory, createMemoryHisto
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Footer',//命名式路由模式
+    // redirect: 'Login',//字符串形式配置
+    redirect: (to) => {//函数模式（可以传参）
+      console.log(to, 'to==>');
+
+      return {
+        path: '/login',
+        query: to.query //将父级参数传递
+      }
+    },
+    alias: ['/root', '/root1'],
     component: () => import('../components/Footer.vue'),//lazy 懒加载
     children: [
       {
-        path: '',
+        path: 'login', //默认路由
         name: 'Login',//命名式路由模式
         component: () => import('../components/Login.vue') //lazy 懒加载
       }, {
