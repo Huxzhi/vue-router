751dc24c3f577c1746bfbec99b78df6a11e67459
Huxzhi <huxzhi@gmail.com>
2023-01-15 15:08:45 +0800

    嵌套路由

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+
+  <div>
+    <hr>
+    <h1>我是父路由</h1>
+    <router-view></router-view>
+  </div>
+
+</template>
+
+<script setup lang='ts'>
+import { ref, reactive } from 'vue'
+
+</script>
+
+<style lang='less' scoped>
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -8,15 +8,23 @@ import { createRouter, createWebHistory, createWebHashHistory, createMemoryHisto
 //路由数组的类型 RouteRecordRaw
 // 定义一些路由
 // 每个路由都需要映射到一个组件。
-const routes: Array<RouteRecordRaw> = [{
-  path: '/',
-  name: 'Login',//命名式路由模式
-  component: () => import('../components/Login.vue') //lazy 懒加载
-}, {
-  path: '/register/:id',
-  name: 'Register',
-  component: () => import('../components/Register.vue')
-}]
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Footer',//命名式路由模式
+    component: () => import('../components/Footer.vue'),//lazy 懒加载
+    children: [
+      {
+        path: '',
+        name: 'Login',//命名式路由模式
+        component: () => import('../components/Login.vue') //lazy 懒加载
+      }, {
+        path: '/register/:id',
+        name: 'Register',
+        component: () => import('../components/Register.vue')
+      }]
+  },
+]
 
 
 
